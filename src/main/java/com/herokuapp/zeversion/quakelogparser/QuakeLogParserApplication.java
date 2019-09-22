91116f9624344a70926fd18d2d1cf64251772a81
Everson <everson___@hotmail.com>
2019-09-22 16:20:07 -0300

    init project

---
+++ src/main/java/com/herokuapp/zeversion/quakelogparser/QuakeLogParserApplication.java
@@ -0,0 +1,13 @@
+package com.herokuapp.zeversion.quakelogparser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuakeLogParserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuakeLogParserApplication.class, args);
+	}
+
+}
